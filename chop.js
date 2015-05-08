26579c4a9a4fa6e07e86a53a19beadd198499cdd
Alex Canales <a.canales@laposte.net>
2015-05-08 18:01:29 +0000

    Add the documentation.

--- chop.js
+++ chop.js
@@ -1,4 +1,4 @@
-//Global variables definition:
+//Global variables definition (initialized in the function "initialize"):
 var bitDiameter;
 var safeZ;
 var angle;
@@ -10,9 +10,13 @@ var cutHeight;
 var cutNumber;
 var canvas;
 var ctx;
-// var inToPx=10;  //Define how much pixel a inch is equal
 var cutBack = true;
 
+/**
+ * Checks if an input text element contains a float. Corrects the element.
+ *
+ * @param {object} element Input text element to check.
+ */
 function checkFloat(element) {
     if(isNaN(parseFloat(element.value)))
         element.value = 0;
@@ -21,14 +25,31 @@ function checkFloat(element) {
 }
 
 
+/**
+ * Converts inches into millimeters.
+ *
+ * @param {number} inches The number of inches.
+ * @return {number} The number of inches converted in millimeter.
+ */
 function convertInToMm(inches) {
     return (isNaN(inches)) ? 0.0 : (parseFloat(inches) * 25.4);
 }
 
+/**
+ * Converts millimeters into inches.
+ *
+ * @param {number} millimeters The number of millimeters.
+ * @return {number} The number of millimeters converted in inch.
+ */
 function convertMmToIn(millimeters) {
     return (isNaN(millimeters)) ? 0.0 : (parseFloat(millimeters) / 25.4);
 }
 
+/**
+ * Gets the number of pixel for an inch.
+ *
+ * @return {number} The number of pixel for an inch.
+ */
 function inchToPixel() {
     var itp = parseFloat(document.getElementById("in_to_px").value);
     if(isNaN(itp))
@@ -36,97 +57,12 @@ function inchToPixel() {
     return itp;
 }
 
-function draw() {
-    var vtp = 0;  //Value To Pixel
-    var margin = 10;
-    var bottom = parseInt(canvas.height) - margin; //To have the y go up and (0,0) is in the bottom left corner
-    var xFront, yFront, xBack, yBack;
-    var halfBit;  //The half of the bit diameter
-    var shift = 0, rise = 0; //hence representation of the mitter when angle > 0
-
-    if(document.getElementById("unit_in").checked == true)
-        vtp = inchToPixel();
-    else
-        vtp = convertMmToIn(inchToPixel());
-
-    if(cutBack)
-        calculateFrontLength();
-    else
-        calculateBackLength();
-
-    xFront = margin + parseFloat(frontLength.value) * vtp;
-    yFront = bottom;
-    xBack = margin + parseFloat(backLength.value) * vtp;
-    yBack = bottom - (parseFloat(boardLength.value) * vtp);
-    halfBit = (parseFloat(bitDiameter.value)) / 2 * vtp;
-
-    ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-
-    //Drawing the back
-    ctx.beginPath();
-    ctx.moveTo(margin, yBack);
-    ctx.lineTo(xBack, yBack);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = "#0000FF";
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(xBack, yBack, 3, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "blue";
-    ctx.fill();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#000033";
-    ctx.stroke();
-
-    //Drawing the front
-    ctx.beginPath();
-    ctx.moveTo(margin, yFront);
-    ctx.lineTo(xFront, yFront);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = "#00FF00";
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(xFront, yFront, 3, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#003300";
-    ctx.stroke();
-
-    //Drawing the board
-    ctx.beginPath();
-    ctx.moveTo(margin, yBack);
-    ctx.lineTo(margin, yFront);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = "#000000";
-    ctx.stroke();
-
-    //Drawing the mitter
-    ctx.beginPath();
-    if(document.getElementById("cut_pos_right").checked == true) {
-        //No need to mutiply by vtp, halfBit is already converted
-        shift = halfBit*Math.cos(parseFloat(angle.value)*Math.PI/180);
-        rise = halfBit*Math.sin(parseFloat(angle.value)*Math.PI/180);
-        if(document.getElementById("tilt_left").checked == true) {
-            rise *= -1;
-        }
-    }
-    ctx.moveTo(xBack + shift, yBack + rise);
-    ctx.lineTo(xFront + shift, yFront + rise);
-    ctx.lineWidth = parseFloat(bitDiameter.value) * vtp;
-    ctx.strokeStyle = "#FF0000";
-    ctx.stroke();
-
-    //Drawing the cut
-    ctx.beginPath();
-    ctx.moveTo(xBack, yBack);
-    ctx.lineTo(xFront, yFront);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#000000";
-    ctx.stroke();
-}
-
-//Calculate the difference between the front end and the back end
-//Return false if error
+/**
+ * Calculates the difference between the front end and the back end.
+ *
+ * @return {number|boolean} The length of the horizontal difference between the
+ *  back edge and the front edge. Returns false if an error occured.
+ */
 function calculateBackFrontDifference() {
     var a = parseFloat(angle.value), b = parseFloat(boardLength.value);
     var f = parseFloat(frontLength.value);
@@ -137,7 +73,11 @@ function calculateBackFrontDifference() {
     return Math.tan(parseFloat(a) * Math.PI/180) * parseFloat(b);
 }
 
-//Calculate the back length according to the angle and the front length
+/**
+ * Calculates the back length according to the angle and the front length.
+ * Corrects directly the input text element.
+ *
+ */
 function calculateBackLength() {
     var difference = calculateBackFrontDifference();
     //frontLength was checked in calculateBackFrontDifference
@@ -150,7 +90,11 @@ function calculateBackLength() {
         backLength.value = parseFloat(frontLength.value) - difference;
 }
 
-//Calculate the back length according to the angle and the front length
+/**
+ * Calculates the back length according to the angle and the front length.
+ * Corrects directly the input text element.
+ *
+ */
 function calculateFrontLength() {
     var difference = calculateBackFrontDifference();
     //frontLength was checked in calculateBackFrontDifference
@@ -163,6 +107,108 @@ function calculateFrontLength() {
         frontLength.value = parseFloat(backLength.value) + difference;
 }
 
+/**
+ * Draws the representation of the cut and board.
+ *
+ */
+function draw() {
+    var vtp = 0;  //Value To Pixel
+    var margin = 10;
+    var bottom = parseInt(canvas.height) - margin; //To have the y go up and (0,0) is in the bottom left corner
+    var xFront, yFront, xBack, yBack; //For the front and back edges
+    var halfBit;  //The half of the bit diameter
+    var shift = 0, rise = 0; //hence representation of the mitter when angle > 0 and cut position on the right
+
+    if(document.getElementById("unit_in").checked == true)
+        vtp = inchToPixel();
+    else
+        vtp = convertMmToIn(inchToPixel());
+
+    if(cutBack)
+        calculateFrontLength();
+    else
+        calculateBackLength();
+
+    //Initializes according to the margin and the number of pixel for an inch
+    xFront = margin + parseFloat(frontLength.value) * vtp;
+    yFront = bottom;
+    xBack = margin + parseFloat(backLength.value) * vtp;
+    yBack = bottom - (parseFloat(boardLength.value) * vtp);
+    halfBit = (parseFloat(bitDiameter.value)) / 2 * vtp;
+
+    ctx.clearRect(0 , 0 , canvas.width, canvas.height);  //Erase the old stuff
+
+    //Drawing the back edge
+    ctx.beginPath();
+    ctx.moveTo(margin, yBack);
+    ctx.lineTo(xBack, yBack);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#0000FF";
+    ctx.stroke();
+
+    //Drawing the front edge
+    ctx.beginPath();
+    ctx.moveTo(margin, yFront);
+    ctx.lineTo(xFront, yFront);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#00FF00";
+    ctx.stroke();
+
+    //Drawing the board
+    ctx.beginPath();
+    ctx.moveTo(margin, yBack);
+    ctx.lineTo(margin, yFront);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#000000";
+    ctx.stroke();
+
+    //Drawing the mitter
+    if(document.getElementById("cut_pos_right").checked == true) {
+        //No need to mutiply by vtp, halfBit is already converted
+        shift = halfBit * Math.cos(parseFloat(angle.value)*Math.PI/180);
+        rise = halfBit * Math.sin(parseFloat(angle.value)*Math.PI/180);
+        if(document.getElementById("tilt_left").checked == true) {
+            rise *= -1;
+        }
+    }
+    ctx.beginPath();
+    ctx.moveTo(xBack + shift, yBack + rise);
+    ctx.lineTo(xFront + shift, yFront + rise);
+    ctx.lineWidth = parseFloat(bitDiameter.value) * vtp;
+    ctx.strokeStyle = "#FF0000";
+    ctx.stroke();
+
+    //Drawing the dot for the back edge
+    ctx.beginPath();
+    ctx.arc(xBack, yBack, 3, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#000033";
+    ctx.stroke();
+
+    //Drawing the dot for the front edge
+    ctx.beginPath();
+    ctx.arc(xFront, yFront, 3, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#003300";
+    ctx.stroke();
+
+    //Drawing the cut
+    ctx.beginPath();
+    ctx.moveTo(xBack, yBack);
+    ctx.lineTo(xFront, yFront);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#000000";
+    ctx.stroke();
+}
+
+/**
+ * Initializes the script.
+ *
+ */
 function initialize() {
     bitDiameter= document.getElementById("bit_diameter");
     safeZ = document.getElementById("safe_z");
