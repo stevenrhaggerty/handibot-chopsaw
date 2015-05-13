70de1cd2716c83511e0a368c22b7f6401b41b888
Alex Canales <a.canales@laposte.net>
2015-05-13 10:22:47 +0000

    Integrate Foundation framework.

--- js/chop.js
+++ js/chop.js
@@ -335,7 +335,7 @@ function initialize() {
 
     canvas = document.getElementById("canvas");
     canvas.height = document.getElementById("form_chop").clientHeight;
-    canvas.width = parseInt(document.getElementById("form_chop").clientWidth)*2;
+    // canvas.width = parseInt(document.getElementById("form_chop").clientWidth)*2;
     ctx = canvas.getContext("2d");
 
     frontLength.disabled = cutBack;
